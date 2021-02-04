19ff9e08c0f69dc53e171151c2c8cec056c4d7b9
Developer <elov62@gmail.com>
2021-02-04 18:45:37 +0500

    lesson6

---
+++ app/src/main/java/geekbarains/material/ui/picture/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,41 @@
+package geekbarains.material.ui.picture
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import geekbarains.material.R
+import geekbarains.material.ui.recycler.RecyclerActivity
+import kotlinx.android.synthetic.main.bottom_navigation_layout.*
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_navigation_layout, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_one -> activity?.let {
+                    startActivity(Intent(it, RecyclerActivity::class.java
+                        )
+                    )
+                }
+
+                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+            }
+            dismiss()
+            true
+        }
+    }
+}
